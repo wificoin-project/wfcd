26155eae15568860ab13d000cdb4bdf2dbedd360
liudengfeng <liudengfeng@kunteng.org>
2018-11-21 18:03:43 +0800

    change testnet powlimit
    Signed-off-by: liudengfeng <liudengfeng@kunteng.org>

--- chaincfg/params.go
+++ chaincfg/params.go
@@ -392,7 +392,7 @@ var TestNet3Params = Params{
 	GenesisBlock:             &testNet3GenesisBlock,
 	GenesisHash:              &testNet3GenesisHash,
 	PowLimit:                 testNet3PowLimit,
-	PowLimitBits:             0x1f00ffff,
+	PowLimitBits:             0x1f0fffff,
 	BIP0034Height:            21111,  // 0000000023b3a96d3484e5abb3755c413e7d41500f8e2a5c3f0dd01299cd8ef8
 	BIP0065Height:            581885, // 00000000007f6655f22f98e72ed80d8b06dc761d5da09df0fa1dc4be4f861eb6
 	BIP0066Height:            330776, // 000000002104c8c45e99a8853285a3b592602a3ccde2b832481da85e9e4ba182
